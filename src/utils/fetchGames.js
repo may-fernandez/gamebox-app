947fc718bfd3b1827222d20bbd8d5e4524d99383
Mayra <mayra.bf3@gmail.com>
2025-08-04 16:48:32 -0300

    fetchGames & .env

---
+++ src/utils/fetchGames.js
@@ -0,0 +1,24 @@
+const BASE_URL= "https://api.rawg.io/api/games";
+const API_KEY= import.meta.env.VITE_RAWG_API_KEY;
+
+const fetchGames = async (params = "") => {
+
+    try{
+        const response = await fetch(`${BASE_URL}?key=${API_KEY}&${params}`);
+        const data = await response.json();
+
+        return data.results.map((game) => ({
+            id: game.id,
+            name: game.name,
+            image: game.background_image,
+            rating: game.rating,
+            genres: game.genres.map((g) => game.name),
+        }));
+    }
+    catch(error){
+        console.error("Error al buscar juegos", error);
+        return [];
+    }
+}
+
+export default fetchGames;
