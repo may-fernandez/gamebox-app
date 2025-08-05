d6479c1bd266b1c41bd10a523966d8fa0c3972f6
Mayra <mayra.bf3@gmail.com>
2025-08-04 22:25:42 -0300

    update home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,38 @@ import './Home.css';
 import Carrousel from '../../components/Carrousel.jsx';
 import {fetchGames} from '../../utils/fetchGames.js';
 
-const Home = () => {}
+const Home = () => {
+    const [topDeLaSemana, setTopDeLaSemana] = useState([]);
+    const [topDelAnio, setTopDelAnio] = useState([]);
+    const [topDeSiempre, setTopDeSiempre] = useState([]);
+
+    useEffect(() => {
+
+        const getGames = async () => {
+            const hoy = new Date();
+            const comienzoAnio = `${hoy.getFullYear()}-01-01`;
+            const finAnio = `${hoy.getFullYear}-12-31`;
+
+            const juegosTopDeLaSemana = await fetchGames("ordering=-added&page_size=10");
+            const juegosTopDelAnio = await fetchGames(`dates=${comienzoAnio},${finAnio}=ordering=-rating&page_size=10`);
+            const juegosTopDeSiempre = await fetchGames("ordering=-rating&page_size=10");
+
+            setTopDeLaSemana(juegosTopDeLaSemana);
+            setTopDelAnio(juegosTopDelAnio);
+            setTopDeSiempre(juegosTopDeSiempre);
+        }
+
+        getGames();
+    }, []);
+
+    return(
+        <div className="home">
+            <Carrousel title="Top de la semana" games={topDeLaSemana}/>
+            <Carrousel title="Top del año" games={topDelAnio}/>
+            <Carrousel title="Favoritos de siempre" games={topDeSiempre}/>
+        </div>
+    );
+
+}
 
 export default Home;
