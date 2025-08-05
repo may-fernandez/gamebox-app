82600126249f005fa3a8b6957d80d492e1a05f52
Mayra <mayra.bf3@gmail.com>
2025-08-04 21:20:34 -0300

    update Home.jsx

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from "react";
+import './Home.css';
+import Carrousel from '../../components/Carrousel.jsx';
+import {fetchGames} from '../../utils/fetchGames.js';
+
+const Home = () => {}
+
+export default Home;
