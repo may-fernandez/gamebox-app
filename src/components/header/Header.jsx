7b010633d23e0300a2d292644b4ab7b25f507931
Mayra <mayra.bf3@gmail.com>
2025-08-07 15:26:23 -0300

    update header & assets

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import 'Header.css';
+import React from "react";
+import "Header.css";
+import search from "../../assets/search.svg";
+import { Link } from "react-router-dom";
 
-function Header(){
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <Link to="/" className="logo"></Link>
+        <Link to="">All games</Link>
 
-    return(
-        <header></header>
-    )
+        <div className="search-bar-container">
+          <input
+            type="text"
+            placeholder="Busca un juego..."
+            className="search-bar"
+          />
+          <button className="btn-buscar">
+            <img src={search} className="search-icon"/>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
